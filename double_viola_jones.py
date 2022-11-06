517ae390640ed3973364168b061c4847b204e717
Matjaz12 <matjaz.muc12@gmail.com>
2022-11-06 22:26:30 +0100

    Add results

---
+++ double_viola_jones.py
@@ -0,0 +1,115 @@
+import cv2
+import numpy as np
+import numpy.typing as npt
+from numba import njit
+
+from load_data import load_data_pickle
+from visualize import show_detection
+
+
+class DoubleViolaJones:
+    def __init__(self, cascade1_path: str, cascade2_path: str, scale_factor: float = None, min_neighbour: float = None,
+                 min_size: float = None, max_size: float = None):
+
+        self.cascade1 = cv2.CascadeClassifier(cascade1_path)
+        self.cascade2 = cv2.CascadeClassifier(cascade2_path)
+
+        self.scale_factor = scale_factor
+        self.min_neighbour = min_neighbour
+        self.min_size = min_size
+        self.max_size = max_size
+
+    @staticmethod
+    def convert_detection(image_height: int, image_width: int, detection: npt.NDArray) -> npt.NDArray:
+        """
+        Convert detection by cv2 viola jones algorithm
+        @param image_height: height of the input image
+        @param image_width: width of the input image
+        @param detection: Detection returned by cv2 viola jones algorithm,
+            [x_up, y_up, box_width, box_height]
+        @return:
+            Detection in the following format
+            [x_center, y_center, box_width, box_height]
+        """
+        x_up, y_up, box_width, box_height = detection
+
+        x_center = x_up + box_width // 2
+        y_center = y_up + box_height // 2
+        x_center /= image_width
+        y_center /= image_height
+        box_width /= image_width
+        box_height /= image_height
+
+        converted_detection = np.array([x_center, y_center, box_width, box_height])
+        return converted_detection
+
+    def predict(self, images: npt.NDArray, convert_coordinates: bool = True) -> npt.NDArray:
+        """
+        Function predicts the bounding boxes.
+        @param images: Input images
+        @param convert_coordinates: Convert predicted coordinates or not.
+        @return: List of predictions:
+            [[sample_idx, predicted_class, predicted_prob, bounding_box], ...]
+        """
+
+        # Compute detections for all images
+        predictions = []
+        for sample_idx, image in enumerate(images):
+
+            # Apply cascade 1
+            detections1 = self.cascade1.detectMultiScale(image, scaleFactor=self.scale_factor,
+                                                        minNeighbors=self.min_neighbour, minSize=self.min_size,
+                                                        maxSize=self.max_size)
+            # Apply cascade 2
+            detections2 = self.cascade2.detectMultiScale(image, scaleFactor=self.scale_factor,
+                                                        minNeighbors=self.min_neighbour, minSize=self.min_size,
+                                                        maxSize=self.max_size)
+
+            if len(detections1) > 0 and len(detections2) > 0:
+                detections = np.concatenate(
+                    (detections1, detections2),
+                    axis=0
+                )
+
+            elif len(detections1) > 0:
+                detections = detections1
+
+            else:
+                detections = detections2
+
+            # Iterate over detections and construct prediction vector
+            for detection in detections:
+                predicted_class = 0
+                predicted_prob = self.min_neighbour if self.min_neighbour is not None else -1.0
+                prediction = [sample_idx, predicted_class, predicted_prob]
+
+                if convert_coordinates:
+                    image_height, image_width = image.shape
+                    detection = DoubleViolaJones.convert_detection(image_height, image_width, detection)
+
+                prediction.extend(detection)
+
+                # Store prediction
+                predictions.append(prediction)
+
+        predictions = np.array(predictions)
+        return predictions
+
+    def __str__(self):
+        return "DoubleViolaJones"
+
+
+if __name__ == "__main__":
+    X_test, y_test = load_data_pickle("./ear_data/X_test_and_y_test.pickle")
+    viola_jones = DoubleViolaJones("./weights/haarcascade_mcs_rightear.xml",
+                                   "./weights/haarcascade_mcs_leftear.xml",
+                                   min_neighbour=3)
+    predictions = viola_jones.predict(X_test)
+
+    # Show ground truth and prediction for a random sample.
+    index_to_show = int(np.random.choice(predictions[:, 0]))
+    pred = predictions[np.where(predictions[:, 0] == index_to_show)]
+    show_detection(X_test[index_to_show],
+                   y_test[index_to_show],
+                   pred,
+                   figure_name=str(viola_jones))
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from collections import OrderedDict, namedtuple
+from datetime import datetime
+from itertools import product
+
 import cv2
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
+from double_viola_jones import DoubleViolaJones
 from evaluation import intersection_over_union, mean_intersection_over_union, intersection_over_union_vector
 from load_data import *
 import matplotlib.pyplot as plt
@@ -12,13 +19,33 @@ from enum import Enum
 
 class Task(Enum):
     VJ_MEAN_IOU = 0
-    YOLO_MEAN_IOU = 1
-    VJ_SHOW_RANDOM_PREDICTION = 2
-    YOLO_SHOW_RANDOM_PREDICTION = 3
-    VJ_FINE_TUNE = 4
-    VJ_SHOW_BEST_PREDICTIONS = 5
-    YOLO_SHOW_BEST_PREDICTIONS = 6
-    VJ_FAILED_PREDICTIONS = 7
+    VJ_DOUBLE_MEAN_IOU = 1
+    YOLO_MEAN_IOU = 2
+    VJ_SHOW_RANDOM_PREDICTION = 3
+    YOLO_SHOW_RANDOM_PREDICTION = 4
+    VJ_FINE_TUNE = 5
+    VJ_SHOW_BEST_PREDICTIONS = 6
+    YOLO_SHOW_BEST_PREDICTIONS = 7
+    VJ_FAILED_PREDICTIONS = 8
+    VJ_DOUBLE_FINE_TUNE = 9
+
+
+class RunBuilder:
+    @staticmethod
+    def get_runs(parameters: OrderedDict):
+        """
+        Function returns all permutations of parameters.
+        :param parameters: Dictionary of parameters
+        :return: List of all parameter permutations.
+        """
+
+        Run = namedtuple("Run", parameters.keys())
+        runs = []
+
+        for val in product(*parameters.values()):
+            runs.append(Run(*val))
+
+        return runs
 
 
 class TaskRunner:
@@ -27,8 +54,8 @@ class TaskRunner:
         X_test, y_test = load_data_pickle("./ear_data/X_test_and_y_test.pickle")
 
         if task == Task.VJ_MEAN_IOU:
-            viola_jones = ViolaJones("./weights/haarcascade_mcs_rightear.xml", min_neighbour=3)
-            # viola_jones = ViolaJones("./weights/haarcascade_mcs_leftear.xml", min_neighbour=3)
+            # viola_jones = ViolaJones("./weights/haarcascade_mcs_rightear.xml")
+            viola_jones = ViolaJones("./weights/haarcascade_mcs_leftear.xml")
 
             predictions = viola_jones.predict(X_test)
             mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
@@ -39,6 +66,16 @@ class TaskRunner:
             predictions = yolo.predict(X_test)
             mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
             print(f"mean_iou: {mean_iou}")
+
+        elif task == Task.VJ_DOUBLE_MEAN_IOU:
+            X_test, y_test = load_data_pickle("./ear_data/X_test_and_y_test.pickle")
+
+            viola_jones = DoubleViolaJones("./weights/haarcascade_mcs_rightear.xml",
+                                           "./weights/haarcascade_mcs_leftear.xml")
+            predictions = viola_jones.predict(X_test)
+            mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
+            print(f"mean_iou: {mean_iou}")
+
 
         elif task == Task.VJ_SHOW_RANDOM_PREDICTION:
             # Show ground truth and prediction for a random sample.
@@ -119,6 +156,79 @@ class TaskRunner:
                                   iou_s_sorted[0:TOP_N], 5, 2,
                                   figure_name="vj_worst6_predictions")
 
+        elif task == Task.VJ_FINE_TUNE:
+            hyper_parameters = OrderedDict(
+                scale_factor=[1.05, 1.1, 1.2, 1.3],
+                min_neighbors=[3, 4, 5, 6, 7]
+            )
+
+            run_data = []
+            run_time = datetime.now()
+            run_params = RunBuilder.get_runs(hyper_parameters)
+
+            # Try each combination of parameters
+            print(f"Running {len(run_params)} experiments...")
+
+            for param_set in tqdm(run_params):
+                viola_jones = ViolaJones(cascade_path="./weights/haarcascade_mcs_leftear.xml",
+                                         scale_factor=param_set.scale_factor,
+                                         min_neighbour=param_set.min_neighbors)
+
+                predictions = viola_jones.predict(X_test)
+                mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
+                print(f"mean_iou: {mean_iou}")
+
+                results = OrderedDict()
+                results["method"] = str(viola_jones)
+                results["scale_factor"] = param_set.scale_factor
+                results["min_neighbors"] = param_set.min_neighbors
+                results["mean_iou"] = mean_iou
+
+                run_data.append(results)
+
+            run_data_df = pd.DataFrame.from_dict(run_data, orient="columns")
+            run_data_df = run_data_df.sort_values("mean_iou", ascending=False)
+            print(run_data_df)
+
+            run_data_df.to_csv(f"./results/{viola_jones}_run_{str(run_time).replace(' ', '_')}", index=False)
+
+        elif task == Task.VJ_DOUBLE_FINE_TUNE:
+            hyper_parameters = OrderedDict(
+                scale_factor=[1.05, 1.1, 1.2, 1.3],
+                min_neighbors=[3, 4, 5, 6, 7]
+            )
+
+            run_data = []
+            run_time = datetime.now()
+            run_params = RunBuilder.get_runs(hyper_parameters)
+
+            # Try each combination of parameters
+            print(f"Running {len(run_params)} experiments...")
+
+            for param_set in tqdm(run_params):
+                viola_jones = DoubleViolaJones("./weights/haarcascade_mcs_rightear.xml",
+                                               "./weights/haarcascade_mcs_leftear.xml",
+                                               scale_factor=param_set.scale_factor,
+                                               min_neighbour=param_set.min_neighbors)
+
+                predictions = viola_jones.predict(X_test)
+                mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
+                print(f"mean_iou: {mean_iou}")
+
+                results = OrderedDict()
+                results["method"] = str(viola_jones)
+                results["scale_factor"] = param_set.scale_factor
+                results["min_neighbors"] = param_set.min_neighbors
+                results["mean_iou"] = mean_iou
+
+                run_data.append(results)
+
+            run_data_df = pd.DataFrame.from_dict(run_data, orient="columns")
+            run_data_df = run_data_df.sort_values("mean_iou", ascending=False)
+            print(run_data_df)
+
+            run_data_df.to_csv(f"./results/{viola_jones}_run_{str(run_time).replace(' ', '_')}", index=False)
+
 
 if __name__ == "__main__":
-    TaskRunner.run(Task.YOLO_MEAN_IOU)
+    TaskRunner.run(Task.VJ_DOUBLE_FINE_TUNE)
