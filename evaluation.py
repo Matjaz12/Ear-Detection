Correct mAP calculation

evaluation.py
@@ -182,6 +182,7 @@ def precision_recall_curve_fixed_threshold(images: npt.NDArray, ground_truths: n
     # Sort by confidence
     iou_conf_vector = iou_conf_vector[iou_conf_vector[:, -1].argsort()[::-1]]
 
+    """
     from scipy.stats import pearsonr
     corr, _ = pearsonr(iou_conf_vector[:, 1], iou_conf_vector[:, 2] / np.max(iou_conf_vector[:, 2]))
     plt.style.use(['science','ieee', 'notebook', 'grid'])
@@ -192,9 +193,21 @@ def precision_recall_curve_fixed_threshold(images: npt.NDArray, ground_truths: n
     plt.title(f"IoU vs Confidence plot, correlation: {np.round(corr, 2)}")
     plt.savefig("./img/yolo_iou_vs_confidence.pdf")
     plt.show()
+    """
 
     # Use iou_threshold to determine FP or TP
-    tp_vector = np.expand_dims(iou_conf_vector[:, 1] >= iou_threshold, axis=1)
+    # Take into account that there can only be one TP in each sample image
+    tp_vector = np.zeros(iou_conf_vector.shape[0])
+    tp_idxes = set()
+
+    for i in range(iou_conf_vector.shape[0]):
+        sample_idx, sample_iou, sample_conf = iou_conf_vector[i]
+
+        if sample_iou > iou_threshold and sample_idx not in tp_idxes:
+            tp_vector[i] = 1
+            tp_idxes.add(sample_idx)
+
+    tp_vector = np.expand_dims(tp_vector, axis=1)
     fp_vector = np.logical_not(tp_vector)
 
     iou_conf_vector = np.concatenate(
@@ -206,6 +219,11 @@ def precision_recall_curve_fixed_threshold(images: npt.NDArray, ground_truths: n
         (iou_conf_vector, fp_vector),
         axis=1
     )
+
+    for i in range(iou_conf_vector.shape[0]):
+        tp_sum_for_index_i = iou_conf_vector[np.where(iou_conf_vector[:, 0] == i)][:, 3].sum()
+        if tp_sum_for_index_i > 1.0:
+            raise Exception("Each sample should have at most #ground truths (1) TPs")
 
     tp_csum_vector = np.expand_dims(np.cumsum(iou_conf_vector[:, -2]), axis=1)
     fp_csum_vector = np.expand_dims(np.cumsum(iou_conf_vector[:, -1]), axis=1)
@@ -327,16 +345,18 @@ def mean_accuracy_precision(images: npt.NDArray, ground_truths: npt.NDArray,
 if __name__ == "__main__":
 
     X_test, y_test = load_data_pickle("./ear_data/X_test_and_y_test_GRAY.pickle")
-    """
 
     viola_jones = DoubleViolaJones("./weights/haarcascade_mcs_rightear.xml",
-                                "./weights/haarcascade_mcs_leftear.xml")
+                                "./weights/haarcascade_mcs_leftear.xml",
+                                min_neighbour=3)
     predictions = viola_jones.predict(X_test)
+    recall, precision = precision_recall_curve_fixed_threshold(X_test, y_test, predictions)
+    plot_precision_recall_curve(recall, precision, figure_title="new_pr_curve_implementation")
 
     """
-
     yolo = YOLO("./weights/yolo5s.pt")
     predictions = yolo.predict(X_test)
 
     recall, precision = precision_recall_curve_fixed_threshold(X_test, y_test, predictions)
     plot_precision_recall_curve(recall, precision)
+    """

main.py
@@ -68,8 +68,8 @@ class TaskRunner:
         X_test, y_test = load_data_pickle(f"./ear_data/X_test_and_y_test_{IMAGE_MODE}.pickle")
 
         if task == Task.VJ_MEAN_IOU:
-            # viola_jones = ViolaJones("./weights/haarcascade_mcs_rightear.xml")
-            viola_jones = ViolaJones("./weights/haarcascade_mcs_leftear.xml")
+            # viola_jones = ViolaJones(LEFT_CASCADE)
+            viola_jones = ViolaJones(RIGHT_CASCADE)
 
             predictions = viola_jones.predict(X_test)
             mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
@@ -83,17 +83,14 @@ class TaskRunner:
 
         elif task == Task.VJ_DOUBLE_MEAN_IOU:
             X_test, y_test = load_data_pickle("./ear_data/X_test_and_y_test.pickle")
-
-            viola_jones = DoubleViolaJones("./weights/haarcascade_mcs_rightear.xml",
-                                           "./weights/haarcascade_mcs_leftear.xml")
+            viola_jones = DoubleViolaJones(RIGHT_CASCADE, LEFT_CASCADE)
             predictions = viola_jones.predict(X_test)
             mean_iou = mean_intersection_over_union(X_test, y_test, predictions)
             print(f"mean_iou: {mean_iou}")
 
         elif task == Task.VJ_SHOW_RANDOM_PREDICTION:
             # Show ground truth and prediction for a random sample.
-            viola_jones = ViolaJones("./weights/haarcascade_mcs_rightear.xml",
-                                     min_neighbour=3)
+            viola_jones = DoubleViolaJones(RIGHT_CASCADE, LEFT_CASCADE)
             predictions = viola_jones.predict(X_test)
 
             index_to_show = int(np.random.choice(predictions[:, 0]))
@@ -265,8 +262,7 @@ class TaskRunner:
 
         elif task == Task.PR_CURVE:
             hyper_parameters=OrderedDict(
-                model=[ViolaJones(LEFT_CASCADE), ViolaJones(RIGHT_CASCADE), 
-                DoubleViolaJones(RIGHT_CASCADE, LEFT_CASCADE), YOLO(YOLO_WEIGHTS)],
+                model=[DoubleViolaJones(RIGHT_CASCADE, LEFT_CASCADE), YOLO(YOLO_WEIGHTS)],
                 iou_threshold=[0.5]
             )
 
@@ -293,8 +289,7 @@ class TaskRunner:
             """
 
             hyper_parameters=OrderedDict(
-                model=[ViolaJones(LEFT_CASCADE), ViolaJones(RIGHT_CASCADE), 
-                DoubleViolaJones(RIGHT_CASCADE, LEFT_CASCADE), YOLO(YOLO_WEIGHTS)],
+                model=[DoubleViolaJones(RIGHT_CASCADE, LEFT_CASCADE), YOLO(YOLO_WEIGHTS)],
                 iou_threshold=[0.5]
             )
 
@@ -322,4 +317,4 @@ class TaskRunner:
             results_df.to_csv(f"./results/mAP_table", index=False)
 
 if __name__ == "__main__":
-    TaskRunner.run(Task.VJ_SHOW_BEST_PREDICTIONS)
+    TaskRunner.run(Task.MAP_TABLE)